88c4d39b1a6499310b3d50f92a134d4eba9fc3e1
HDespain <hiramdespain@gmail.com>
2025-07-02 18:10:17 -0600

    Started burner methods

--- src/Burner.java
+++ src/Burner.java
@@ -1,9 +1,52 @@
 
 public class Burner {
 	private enum Temperature { BLAZING, HOT, WARM, COLD };
-	Temperature myTemperature;
+	private Temperature myTemperature = COLD;
+	private Setting mySetting = OFF;
+	private int timer;
+	private final static int TIME_DURATION = 2;
+
 	public Temperature getMyTemperature() {
 		return myTemperature;
 	}
-	
+
+	public plusButton() {
+		switch (mySetting) {
+		case OFF:
+			mySetting = LOW;
+			break;
+		case LOW:
+			mySetting = MEDIUM;
+			break;
+		case MEDIUM:
+		case HIGH:
+			mySetting = HIGH;
+			break;
+		}
+	}
+
+	public minusButton() {
+		switch (mySetting) {
+		case OFF:
+		case LOW:
+			mySetting = OFF;
+			break;
+		case MEDIUM:
+			mySetting = LOW;
+			break;
+		case HIGH:
+			mySetting = MEDIUM;
+			break;
+		}
+	}
+
+	public updateTemperature() {
+
+	}
+
+	public display() {
+
+	}
+
+
 }
